b24b9f0cda5d33ad7d8ea02b3715063e48a7e033
PierreTzt <76401152+PierreTzt@users.noreply.github.com>
2021-08-12 09:54:16 +0200

    Un peu de design

--- index.php
+++ index.php
@@ -3,27 +3,32 @@
     <head>
         <title>Page de traitement</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" />
     </head>
     <body>
-    <h1>Formulaire HTML</h1>
+    <div class="intro-y box mt-5">
+                                <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+                                    <h2 class="font-medium text-base mr-auto">
         
         <form action="envoi.php" method="post">
-            <div class="c100">
-                <label for="date_commande">Date de commande : </label>
+            <div class="form-inline">
+                <label for="date_commande" class="form-inline">>Date de commande : </label>
                 <input type="date" id="date_achat" name="date_achat">
             </div>
-            <div class="c100">
-                <label for="date_review">Date de review : </label>
+            <div class="form-inline">
+                <label for="date_review" class="form-inline">>Date de review : </label>
                 <input type="date" id="date_review" name="date_review">
             </div>
-            <div class="c100">
-                <label for="review_poste">Avis posté</label>
+            <div class="form-inline">
+                <label for="review_poste" class="form-inline">>Avis posté</label>
                 <input type="text" id="review_poste" name="review_poste">
             </div>
-            <div class="c100" id="submit">
-                <input type="submit" value="Envoyer">
+            <div class="sm:ml-20 sm:pl-5 mt-5" id="submit">
+                <input class="btn btn-primary" type="submit" value="Envoyer">
             </div>
         </form>
+</div>
+</div>
 
 
         <?php
