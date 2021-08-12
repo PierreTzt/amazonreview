0d83da49f45db007b5c59c283cd4ec4325dcee8a
PierreTzt <76401152+PierreTzt@users.noreply.github.com>
2021-08-12 10:32:49 +0200

    v0.2

--- index.php
+++ index.php
@@ -3,29 +3,40 @@
     <head>
         <title>Page de traitement</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css" />
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
     </head>
     <body>
-    <div class="intro-y box mt-5">
-                                <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
-                                    <h2 class="font-medium text-base mr-auto">
+    <div class="col-lg-9 order-1 order-lg-2">
+
+<div class="tab-pane tab-pane-navigation active" id="formsStyleDefault">
+
+    <h4 class="mb-3">Default</h4>
+    <div class="card mb-4">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
         
-        <form action="envoi.php" method="post">
-            <div class="form-inline">
-                <label for="date_commande" class="form-inline">>Date de commande : </label>
+        <form class="contact-form" action="envoi.php" method="post">
+        <div class="row">
+            <div class="form-group col-lg-6">
+                <label for="date_commande" class="form-label mb-1 text-2">>Date de commande : </label>
                 <input type="date" id="date_achat" name="date_achat">
             </div>
-            <div class="form-inline">
+            <div class="form-group col-lg-6">
                 <label for="date_review" class="form-inline">>Date de review : </label>
                 <input type="date" id="date_review" name="date_review">
             </div>
-            <div class="form-inline">
+            <div class="form-group col-lg-6">
                 <label for="review_poste" class="form-inline">>Avis posté</label>
                 <input type="text" id="review_poste" name="review_poste">
             </div>
             <div class="sm:ml-20 sm:pl-5 mt-5" id="submit">
                 <input class="btn btn-primary" type="submit" value="Envoyer">
             </div>
+        </div>
         </form>
 </div>
 </div>
