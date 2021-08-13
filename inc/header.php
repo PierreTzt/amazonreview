4620e7fd24317e17cf3d4a7c8432c8f8bf383342
PierreTzt <76401152+PierreTzt@users.noreply.github.com>
2021-08-13 13:13:09 +0200

    v0.8

--- inc/header.php
+++ inc/header.php
@@ -27,7 +27,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Amazon Review</a>
+            <a class="navbar-brand" href="account.php">Amazon Review</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
