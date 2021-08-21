d8f9cfbcda5e2bd175b81b4123c61f4b5417de00
PierreTzt <76401152+PierreTzt@users.noreply.github.com>
2021-08-21 13:27:26 +0200

    v1.8

--- add_confirm.php
+++ add_confirm.php
@@ -9,13 +9,12 @@ $review_date = $_POST["review_date"];
 $name = $_POST["name"];
 $price = $_POST["price"];
 $url = $_POST["url"];
-$expectancy = $_POST["expectancy"];
 $resale = $_POST["resale"];
 $refund_type = $_POST["refund_type"];
 $frais_paypal = $_POST["frais_paypal"];
 $username = $_POST["username"];
 
-for ($i=0; $i<sizeof ($expectancy);$i++) { 
-    $db = App::getDatabase()->query("INSERT INTO products(name,purchase_date,review_date,price,url,username,refund_type,resale,expectancy) VALUES('$name','$purchase_date','$review_date','$price','$url','$username','$refund_type','$resale','$expectancy[$i]')");
-}
+$expectancy = implode(",",$_POST['expectancy']);
+
+$db = App::getDatabase()->query("INSERT INTO products(name,purchase_date,review_date,price,url,username,refund_type,resale,expectancy) VALUES('$name','$purchase_date','$review_date','$price','$url','$username','$refund_type','$resale','$expectancy')");
 ?>
